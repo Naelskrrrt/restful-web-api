c9f1c7ee34c0fdfca66713679d82e47a7e9713aa
Annaël Horthannice LALASON <140686269+Naelskrrrt@users.noreply.github.com>
2024-05-23 21:13:39 +0300

    Initial commit

---
+++ restful-web-api/src/main/java/com/in28minutes/rest/webservices/restfulwebapi/RestfulWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.rest.webservices.restfulwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebApiApplication.class, args);
+    }
+
+}
---
+++ restful-web-api/src/test/java/com/in28minutes/rest/webservices/restfulwebapi/RestfulWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.rest.webservices.restfulwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
